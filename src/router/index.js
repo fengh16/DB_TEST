894843255f1fa1a38b6fcc6aabe60c064503f7ac
shi-gy15 <thss15_shigy@163.com>
2020-11-14 02:43:30 +0800

    Add relation/manipulation

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AuthRelationPage from '@/pages/Auth_1_Relation'
 import AuthGraphPage from '@/pages/Auth_1_Graph'
+import RelManipulation from '@/pages/relational/RelManipulation'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/graph/Auth_1',
       name: '图权限管理',
       component: AuthGraphPage
+    },
+    {
+      path: '/relation/Operate_4',
+      name: '关系可操作性',
+      component: RelManipulation
     }
   ]
 })
